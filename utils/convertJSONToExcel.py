d74bfb9a7e432d6b99c139bd4d78db8896ae8673
Reju Koshy <reju.koshy@snippetcommerce360.com>
2025-01-27 18:30:06 +0530

    changes

--- utils/convertJSONToExcel.py
+++ utils/convertJSONToExcel.py
@@ -37,7 +37,7 @@ def json_to_excel(json_dir, output_excel_file):
 json_directory = '../output_json'  # Replace with your directory containing JSON files
 
 # Define the output folder
-output_folder = '../output_excel'  # Folder where the Excel file will be saved
+output_folder = '../final-matrixify-export'  # Folder where the Excel file will be saved
 os.makedirs(output_folder, exist_ok=True)  # Create the folder if it doesn't exist
 
 # Get the current date and time to format the file name
