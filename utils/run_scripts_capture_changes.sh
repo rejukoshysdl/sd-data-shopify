91b34e728e8969be96bffcfe0fcd7f381173eaa7
Reju Koshy <reju.koshy@snippetcommerce360.com>
2025-02-10 19:07:43 -0800

    commit

--- utils/run_scripts_capture_changes.sh
+++ utils/run_scripts_capture_changes.sh
@@ -1,10 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 
-#git diff origin/dev dev > ../changes/git-diff/changes.diff //apploicable only after comitting
+# Define an array of files you want to check for changes
+files=("../repo-shopify-data/Pages.json" "../repo-shopify-data/Redirects.json")
 
-git diff > ../changes/git-diff/changes.diff 
+# Initialize an empty string to hold the file list for git diff
+file_list=""
 
-# Execute the  Python script to generate changes
+# Loop through the files array and add each file to the git diff command
+for file in "${files[@]}"; do
+    file_list="$file_list $file"
+done
+
+# Run git diff on the specified files
+git diff  $file_list > ../changes/git-diff/changes.diff
+
+# Execute the Python script to generate changes
+python3.13 extractIdByPage.py
+
+# Execute the Python script to generate changes
 python3.13 extract-changes-only.py
 
 echo "Script execution completed."
